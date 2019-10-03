374bc9019e2bd255b89b27638ba7bd307a1d9975
Rob Price <robprice2798@gmail.com>
2019-10-03 12:15:58 -0400

    Started lab 5, took notes in collision hull

--- Lab1/GamePhysicsLabFrameworkRob/Assets/Scripts/Physics/Collision/CollisionHull2D.cs
+++ Lab1/GamePhysicsLabFrameworkRob/Assets/Scripts/Physics/Collision/CollisionHull2D.cs
@@ -8,16 +8,49 @@ public abstract class CollisionHull2D : MonoBehaviour
     {
         public struct Contact
         {
-            Vector2 point;
-            Vector2 normal;
-            float restitutionCoefficient;
+            public Vector2 point;
+            public Vector2 normal;
+            public float restitutionCoefficient;
         }
         public CollisionHull2D a = null, b = null;
         public Contact[] contact = new Contact[4];
         public int contactCount = 0;
         public bool status = false;
 
+        // Vc = (Va - Vb) * (Direction vector a - direction vector b)
         public Vector2 closingVelocity;
+
+        // Velocity after collision = -(restitution * closing velocity)
+        // Contact normal = Direction vector a - direction vector b
+        // Impulse (g) = m * v
+
+        // Contact Resolver Algorithm
+        // 1. Apply impmulses on objects to simulate bouncing apart
+        // 2. Move objects apart (non-interpenetrating) so they aren't embedded in each other
+        // 3. Check if the contact is resting rather than colliding
+
+        // Resolution order
+        // 1. Calculate the separating velocity of each contact, keeping track of the lowest
+        // 2. If the lowest separating velocity is greater than or equal to 0, exit algorithm, they are resting
+        // 3. Process the contact resolver for the contact with the lowest separating velocity
+        // 4. If there are more contacts, return to step 1
+
+        // POSSIBLE 2ND METHOD
+        // 1. Let the start time be the current simulation time, and the end time be the end
+        //    of the current update request.
+        // 2. Perform a complete update for the entire time interval.
+        // 3. Run the collision detector and collect a list of collisions.
+        // 4. If there are no collisions, we are done: exit the algorithm.
+        // 5. For each collision, work out the exact time of the first collision.
+        // 6. Choose the first collision to have occurred.
+        // 7. If thefirst collision occurs after the end time, then we’re done: exit the algorithm.
+        // 8. Remove the effects of the Step 2 update, and perform a new update from the
+        //    start time to the first collision time.
+        // 9. Process the collision, applying the appropriate impulses (no interpenetration
+        //    resolution is needed, because at the instant of collision the objects are only just
+        //    touching).
+        // 10. Set the start time to be the first collision time, keep the end time unchanged,
+        //     and return to Step 1.
     }
 
     //public List<CollisionHull2D> otherColHullList;
