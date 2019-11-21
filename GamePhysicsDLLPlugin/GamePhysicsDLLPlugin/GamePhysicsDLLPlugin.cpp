2672d3ca1842c02af4ebd12a8f48e46e8de85f66
Cameron Belcher <cameron.belcher@mymail.champlain.edu>
2019-11-21 12:26:40 -0500

    didnt push some stuff still not done

--- GamePhysicsDLLPlugin/GamePhysicsDLLPlugin/GamePhysicsDLLPlugin.cpp
+++ GamePhysicsDLLPlugin/GamePhysicsDLLPlugin/GamePhysicsDLLPlugin.cpp
@@ -7,16 +7,10 @@
 
 
 // This is an example of an exported variable
-GAMEPHYSICSDLL_API int nGamePhysicsDLL=0;
+int nGamePhysicsDLL=0;
 
 // This is an example of an exported function.
-GAMEPHYSICSDLL_API int fnGamePhysicsDLL(void)
+int fnGamePhysicsDLL(void)
 {
     return 0;
-}
-
-// This is the constructor of a class that has been exported.
-GamePhysicsDLL::GamePhysicsDLL()
-{
-    return;
 }
